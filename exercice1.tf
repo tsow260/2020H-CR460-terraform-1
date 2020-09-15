f4a87200ae0b6b3c29b370aacaf1052848b1f44b
matbilodeau <matbilodeau@users.noreply.github.com>
2020-09-15 15:39:01 -0400

    Update exercice1.tf
    dev has custom network

--- exercice1.tf
+++ exercice1.tf
@@ -16,9 +16,13 @@ resource "google_compute_instance" "vm_instance" {
   }
 
   network_interface {
-    # A default network is created for all GCP projects
-    network       = "default"
+    network = google_compute_network.my-network.self_link
     access_config {
     }
   }
+}
+
+resource "google_compute_network" "my-network" {
+  name                    = "terraform-network"
+  auto_create_subnetworks = "true"
 }
